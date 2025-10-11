42dbce7eade753845cf45943e6d7ee0c8ef3a64b
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-10-12 00:45:42 +0900

    chore : setting project and test END

--- sql/test/setting-test.sql
+++ sql/test/setting-test.sql
@@ -1,12 +1,25 @@
-SHOW DATABASES ;
-create table project_spring_jpa_shop.db_setting_test(id int);
+SELECT @@autocommit;
+
+show databases;
 
 show tables from project_spring_jpa_shop;
 
+create table if not exists project_spring_jpa_shop.db_setting_test(id int);
+
+start transaction ;
 insert into project_spring_jpa_shop.db_setting_test values(1);
 insert into project_spring_jpa_shop.db_setting_test values(2);
+commit;
+
+SELECT * FROM project_spring_jpa_shop.db_setting_test;
+
+start transaction ;
 insert into project_spring_jpa_shop.db_setting_test values(3);
+Select * from project_spring_jpa_shop.db_setting_test;
+rollback ;
+Select * from project_spring_jpa_shop.db_setting_test;
 
-select * from project_spring_jpa_shop.db_setting_test;
+drop table if exists project_spring_jpa_shop.db_setting_test ;
 
-select * from project_spring_jpa_shop.user;
+show tables from project_spring_jpa_shop;
+
